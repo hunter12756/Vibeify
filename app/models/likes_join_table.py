c24282960b3124a5393683881998fcfced311d50
hunter12756 <hlwatson17256@gmail.com>
2023-09-12 13:28:53 -0400

    r

--- app/models/likes_join_table.py
+++ app/models/likes_join_table.py
@@ -7,13 +7,13 @@ likes = db.Table(
     db.Column(
         "user_id",
         db.Integer,
-        db.ForeignKey("users.id"),
+        db.ForeignKey(add_prefix_for_prod("users.id")),
         primary_key=True
     ),
     db.Column(
         "song_id",
         db.Integer,
-        db.ForeignKey("songs.id"),
+        db.ForeignKey(add_prefix_for_prod("songs.id")),
         primary_key=True
     )
 )
