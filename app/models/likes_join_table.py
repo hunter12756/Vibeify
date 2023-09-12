97408c790ce765b959eab82dcbd8aa2a3a609e54
hunter12756 <hlwatson17256@gmail.com>
2023-09-12 13:33:43 -0400

    f

--- app/models/likes_join_table.py
+++ app/models/likes_join_table.py
@@ -1,7 +1,8 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 
-
+if environment == "production":
+        __table_args__ = {"schema": SCHEMA}
 likes = db.Table(
     "user_likes",
     db.Column(
