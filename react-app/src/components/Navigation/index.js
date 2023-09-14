56add596e49ee0a2390296c90f8a32e2f3a16bee
hunter12756 <hlwatson17256@gmail.com>
2023-09-14 12:05:52 -0400

    fixed your artist page

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,19 +1,24 @@
 import React, { useState,useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector,useDispatch } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 import OpenModalButton from "../OpenModalButton";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
 import CreateArtist from '../CreateArtistForm';
+import * as artistActions from '../../store/artist'
 function Navigation({ isLoaded }) {
 	const [showMenu, setShowMenu] = useState(false);
 	const [hasArtistPage, setHasArtistPage] = useState(false);
-
+	const dispatch = useDispatch()
 	const sessionUser = useSelector(state => state.session.user);
+	const artists = useSelector(state=>state.artists.allArtists)
+	const filteredArtists = Object.values(artists).filter(artist => artist.user_id === Number(sessionUser.id));
+	const filteredArtist= filteredArtists[0]
+	console.log("FILTERED ARTIST",filteredArtist)
 	const closeMenu = () => setShowMenu(false);
-	
+
 	useEffect(() => {
 		// Make an API request to check if the artist page exists
 		fetch('/api/artists/check-artist')
@@ -24,14 +29,15 @@ function Navigation({ isLoaded }) {
 		  .catch((error) => {
 			console.error('Error checking artist page:', error);
 		  });
-	  }, []);
+		  dispatch(artistActions.getAllArtistsThunk())
+	  }, [dispatch]);
 
 	return (
 		<>
 
 			<div className="navbar">
 
-				{isLoaded && sessionUser ?
+				{isLoaded && sessionUser && filteredArtist ?
 					(
 						<>
 							<div className='logged-in-user-container'>
@@ -40,7 +46,10 @@ function Navigation({ isLoaded }) {
 
 							{hasArtistPage ? (
 								// Content to display when the artist page exists
-								<div>Your artist page content</div>
+								<NavLink to={`/artists/${filteredArtist.id}`}>
+
+									<div>Your artist page </div>
+								</NavLink>
 							) : (
 								// Prompt to create an artist page
 								<OpenModalButton
